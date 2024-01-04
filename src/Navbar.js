4dfd6d99011493be886ca24439d7ccf318895a6e
Euihyun Lee <euihyun.dev@gmail.com>
2024-01-04 08:17:02 +0000

    Make prettier navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -10,25 +10,29 @@ function Navbar({ title, total, managementMenus }) {
   const [offCanvasVisible, setOffCanvasVisible] = useState(false);
   return (
     <>
-    <OffCanvas
-      visible={offCanvasVisible}
-      setVisible={setOffCanvasVisible}
-      managementMenus={managementMenus} />
-    <CNavbar colorScheme="light" className="bg-light">
-      <CCol xs="auto" style={{ display: 'flex' }}>
-        <CIcon
-          icon={cilMenu}
-          size="xl"
-          style={{ marginLeft: '10px', marginRight: '10px' }}
-          onClick={() => setOffCanvasVisible(true)} />
-      </CCol>
-      <CCol>
-        <CNavbarBrand>{title}</CNavbarBrand>
-      </CCol>
-      <CCol style={{ textAlign: 'right' }}>
-        <CNavbarBrand>{total}</CNavbarBrand>
-      </CCol>
-    </CNavbar>
+      <OffCanvas
+        visible={offCanvasVisible}
+        setVisible={setOffCanvasVisible}
+        managementMenus={managementMenus} />
+      <CNavbar
+        colorScheme="light"
+        className="bg-light"
+        style={{ '--cui-navbar-padding-y': '1rem',
+                 '--cui-navbar-padding-x': '1.25rem' }}>
+        <CCol xs="auto" style={{ display: 'flex' }}>
+          <CIcon
+            icon={cilMenu}
+            size="xl"
+            style={{ marginLeft: '0px', marginRight: '0rem' }}
+            onClick={() => setOffCanvasVisible(true)} />
+        </CCol>
+        <CCol>
+          <CNavbarBrand style={{ marginLeft: '1.25rem' }}>{title}</CNavbarBrand>
+        </CCol>
+        <CCol style={{ textAlign: 'right' }}>
+          <CNavbarBrand>{total}</CNavbarBrand>
+        </CCol>
+      </CNavbar>
     </>
   );
 }
